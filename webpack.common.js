c1ee5dae568e2215f2ecb42242da9c05ec5d3141
Andrea Gherardi <andrea@mirrorprod.com>
2020-01-05 10:14:41 +0100

    Disable css url handling

--- webpack.common.js
+++ webpack.common.js
@@ -67,7 +67,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ["vue-style-loader", "css-loader"]
+        use: ["vue-style-loader", "css-loader?url=false"]
       }
     ]
   },
